bfd1277ee3d4b9ecb5eb26da0c0a1c4c801fd97e
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-03-21 19:58:09 -0400

    finished

--- main.js
+++ main.js
@@ -12,19 +12,114 @@ const BALL_SIZE = 20;
 // Get the computer paddle element
 const computerPaddle = document.querySelector('.computer-paddle');
 
+// left paddle element
+const playerPaddle = document.querySelector('.player-paddle');
+
+// Get ball element
+const ball = document.querySelector('.ball');
+
+// Variables to store the ball's x-position, y-position, x-velocity, and y-velocity
+let ballXPosition = 50;
+let ballYPosition = 50;
+let ballXVelocity = 5;
+let ballYVelocity = 5;
+
 // The y-velocity of the computer paddle
 let computerPaddleYPosition = 0;
-let computerPaddleYVelocity = 1;
+let computerPaddleYVelocity = 4;
+
+//Variables for player paddle
+let playerPaddleYPosition = 0;
+let playerPaddleYVelocity = 25;
+
+//Event listener for player up and down movement
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 38) {
+       if (playerPaddleYPosition >= 1){
+           playerPaddleYPosition = playerPaddleYPosition- playerPaddleYVelocity;
+        }
+    } else if (e.keyCode == 40) {
+        if (playerPaddleYPosition <= 380){
+        playerPaddleYPosition = playerPaddleYPosition + playerPaddleYVelocity;
+    }
+    }
+})
 
 // Update the pong world
 function update() {
 
-    // Update the computer paddle's position
-    computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
+    // Apply the y-position for player paddle 
+    playerPaddle.style.top = `${playerPaddleYPosition}px`;
 
+    // Update the computer paddle's position
+    if (ballYPosition >= 0 && ballYPosition <= 400){
+        computerPaddleYPosition = ballYPosition;
+    } 
+    
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
+    computerPaddle.style.bottom = `${computerPaddleYPosition + 100}px`;
+    
+    // Update the ball's y-position 
+    ballXPosition = ballXPosition + ballXVelocity;
+
+    // Update the ball's x-position
+    ballYPosition = ballYPosition + ballYVelocity;
+
+    // Apply the x-position
+    ball.style.left = `${ballXPosition}px`
+    ball.style.right = `${ballXPosition + 20}px`
+
+    // Apply the y-position
+    ball.style.top = `${ballYPosition}px`
+    ball.style.bottom = `${ballYPosition + 20}px`
+
+    // bounce off top and bottom (unless outside of gamearea, in case of goal) 
+    if (ballYPosition <= 0 || ballYPosition >= 480){
+       ballYVelocity = ballYVelocity * -1
+    }
+
+    // Reset ball in center after goal, with 2 second delay to restart
+    if (ballXPosition < 0 || ballXPosition > 680){
+       setTimeout(() => {ballXPosition = 350; ballYPosition = 250;}, 2000);
+    }
+
+    // // make computer paddle bounce off top and bottom
+    // if (computerPaddleYPosition < 0 || computerPaddleYPosition > 400){
+    //     computerPaddleYVelocity = computerPaddleYVelocity * -1;
+    // }
+
+    // make ball bounce off computer paddle
+    if ((ballXPosition + 20) === 680 && ballYPosition > (computerPaddleYPosition - 19) && (ballYPosition < computerPaddleYPosition + 100)){
+        ballXVelocity = ballXVelocity * -1;
+    }
+
+    // make ball bounce off player paddle
+    if (ballXPosition === 20 && ballYPosition > playerPaddleYPosition && (ballYPosition < playerPaddleYPosition + 100)){
+        ballXVelocity = ballXVelocity * -1;
+    }
+
+    // // make ball bounce off top of player paddle
+    // if (ballXPosition < 20 && ballXPosition > 0 && ballYPosition > (playerPaddleYPosition - 20) && ballYPosition <= playerPaddleYPosition){
+    //     ballYVelocity = ballYVelocity * -1;
+    // }
+
+    // // make ball bounce off bottom of player paddle
+    // if (ballXPosition < 20 && ballXPosition > 0 && ballYPosition >= (playerPaddleYPosition + 100) && ballYPosition < (playerPaddleYPosition + 120)){
+    //     ballYVelocity = ballYVelocity * -1;
+    // }
+
+    // // make ball bounce off top of computer paddle
+    // if ((ballXPosition + 20) > 680 && (ballXPosition + 20) < 700 &&ballYPosition === computerPaddleYPosition && (ballYPosition < computerPaddleYPosition + 100)){
+    //     ballYVelocity = ballYVelocity * -1;
+    // }
+
+    // // make ball bounce off bottom of computer paddle
+    // if ((ballXPosition + 20) > 680 && (ballXPosition + 20) < 700 && ballYPosition === (computerPaddleYPosition + 100)){
+    //     ballYVelocity = ballYVelocity * -1;
+    // }
+
 }
 
 // Call the update() function every 35ms
-setInterval(update, 35);
+setInterval(update, 30);
