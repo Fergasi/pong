572d128066578f6e3f37cf00fbd14772ae01a6f6
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-03-31 15:48:33 -0400

    finished

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-// Size of the game area (in px)
-const GAME_AREA_WIDTH = 700;
-const GAME_AREA_HEIGHT = 500;
+let randoCounter = false;
 
 // Size of the paddles (in px)
 const PADDLE_HEIGHT = 100;
@@ -18,14 +16,17 @@ const playerPaddle = document.querySelector('.player-paddle');
 // Get ball element
 const ball = document.querySelector('.ball');
 
+// Get game area element
+const gameArea =  document.querySelector('.game-area');
+
 // Get score elements
 let playerScore = document.querySelector('#Player-1');
 let computerScore = document.querySelector('#Computer');
 
 // Variables to store the ball's x-position, y-position, x-velocity, and y-velocity
-let ballXPosition = 50;
-let ballYPosition = 50;
-let ballXVelocity = 5;
+let ballXPosition = (visualViewport.width * 0.65)/2;
+let ballYPosition = (visualViewport.height * 0.5)/2;
+let ballXVelocity = 8;
 let ballYVelocity = 5;
 
 // The y-velocity of the computer paddle
@@ -34,7 +35,7 @@ let computerPaddleYVelocity = 4;
 
 //Variables for player paddle
 let playerPaddleYPosition = 0;
-let playerPaddleYVelocity = 25;
+let playerPaddleYVelocity = 20;
 
 // Player score counter variables
 playerScoreCount = 0;
@@ -43,12 +44,17 @@ computerScoreCount = 0;
 //Event listener for player up and down movement
 document.addEventListener('keydown', function(e) {
     if (e.key == 'ArrowUp') {
-       if (playerPaddleYPosition >= 1){
-           playerPaddleYPosition = playerPaddleYPosition- playerPaddleYVelocity;
+       if (playerPaddleYPosition >= 20){
+           playerPaddleYPosition = playerPaddleYPosition - playerPaddleYVelocity;
+        } else if (playerPaddleYPosition < 20 && playerPaddleYPosition > 0){
+            playerPaddleYPosition = playerPaddleYPosition - playerPaddleYPosition;
         }
-    } else if (e.key == 'ArrowDown') {
-        if (playerPaddleYPosition <= 380){
+    }
+    if (e.key == 'ArrowDown') {
+        if (playerPaddleYPosition <= (visualViewport.height * 0.5) - PADDLE_HEIGHT - playerPaddleYVelocity){
         playerPaddleYPosition = playerPaddleYPosition + playerPaddleYVelocity;
+    } else if (playerPaddleYPosition > (visualViewport.height * 0.5) - PADDLE_HEIGHT - playerPaddleYVelocity && playerPaddleYPosition < (visualViewport.height * 0.5)){
+        playerPaddleYPosition = playerPaddleYPosition + ((visualViewport.height * 0.5) - playerPaddleYPosition - 100);
     }
     }
 })
@@ -60,9 +66,13 @@ function update() {
     playerPaddle.style.top = `${playerPaddleYPosition}px`;
 
     // Update the computer paddle's position
-    if (ballYPosition >= 0 && ballYPosition <= 400){
-        computerPaddleYPosition = ballYPosition;
-    } 
+    if (ballYPosition >= 40 && ballYPosition <= (visualViewport.height * 0.5) - 60){
+        computerPaddleYPosition = ballYPosition - 40;
+    } else if (ballYPosition < 40){
+        computerPaddleYPosition = 0;
+    } else if (ballYPosition > (visualViewport.height * 0.5) - 60){
+        computerPaddleYPosition = (visualViewport.height * 0.5) - 100;
+    }
     
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
@@ -76,42 +86,46 @@ function update() {
 
     // Apply the x-position
     ball.style.left = `${ballXPosition}px`
-    ball.style.right = `${ballXPosition + 20}px`
+    ball.style.right = `${ballXPosition}px`
 
     // Apply the y-position
     ball.style.top = `${ballYPosition}px`
-    ball.style.bottom = `${ballYPosition + 20}px`
+    ball.style.bottom = `${ballYPosition}px`
 
     // bounce off top and bottom  
-    if (ballYPosition <= 0 || ballYPosition >= 480){
+    if (ballYPosition <= 0 || ballYPosition >= (visualViewport.height * 0.5) - 20){
        ballYVelocity = ballYVelocity * -1;
     }
 
     // Reset ball in center after goal, with 2 second delay to restart
-    if (ballXPosition < 0 || ballXPosition > 680){
-       setTimeout(() => {ballXPosition = 350; ballYPosition = 250;}, 2000);
+    if (ballXPosition < 0 || ballXPosition > (visualViewport.width * 0.65)){
+       setTimeout(() => {ballXPosition = (visualViewport.width * 0.65)/2; ballYPosition = (visualViewport.height * 0.5)/2;}, 2000);
     }
 
     // make ball bounce off computer paddle
-    if ((ballXPosition + 20) === 680 && ballYPosition > (computerPaddleYPosition - 19) && (ballYPosition < computerPaddleYPosition + 100)){
+    if ((ballXPosition + 20) >= (visualViewport.width * 0.65) - 20 && (ballXPosition + 20) < (visualViewport.width * 0.65) && ballYPosition > (computerPaddleYPosition - 19) && (ballYPosition < computerPaddleYPosition + 100)){
         ballXVelocity = ballXVelocity * -1;
     }
 
     // make ball bounce off player paddle
-    if (ballXPosition === 20 && ballYPosition > playerPaddleYPosition && (ballYPosition < playerPaddleYPosition + 100)){
+    if (ballXPosition < 20 && ballXPosition > 0 && ballYPosition > playerPaddleYPosition - 19 && ballYPosition < playerPaddleYPosition + 100){
         ballXVelocity = ballXVelocity * -1;
     }
 
     // Update computer score and change score text dom
-    if (ballXPosition === 0){
+    if (ballXPosition < 0 && randoCounter === false){
+        randoCounter = true;
         computerScoreCount += 1;
         computerScore.innerText = `Computer: ${computerScoreCount}`;
+        setTimeout(() => {randoCounter = false;}, 2000);
     }
 
     // Update player score and change score text dom
-    if (ballXPosition === 700){
+    if (ballXPosition > visualViewport.width * 0.65 && randoCounter === false){
+        randoCounter = true;
         playerScoreCount += 1;
         playerScore.innerText = `Player: ${playerScoreCount}`;
+        setTimeout(() => {randoCounter = false;}, 2000);
     }
 
     // ** STUFF I WAS TOO AMBITIOUS ABOUT **
@@ -145,3 +159,4 @@ function update() {
 
 // Call the update() function every 30ms
 setInterval(update, 30);
+
